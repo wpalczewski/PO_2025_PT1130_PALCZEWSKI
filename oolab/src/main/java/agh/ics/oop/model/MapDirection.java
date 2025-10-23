17ac2e3baaa193a9b5d7f8a96525a11a1fb267f8
Wojciech Palczewski <w.k.palczewski@gmail.com>
2025-10-23 18:24:25 +0200

    Added Vector2d and MapDirection

---
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -0,0 +1,45 @@
+package agh.ics.oop.model;
+
+
+public enum MapDirection {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST;
+    @Override
+    public String toString() {
+        return switch (this){
+            case NORTH -> "Północ";
+            case EAST -> "Wschód";
+            case WEST -> "Zachód";
+            case SOUTH -> "Południe";
+        };
+    }
+    public MapDirection next() {
+        return switch (this) {
+            case NORTH -> EAST;
+            case EAST -> SOUTH;
+            case SOUTH -> WEST;
+            case WEST -> NORTH;
+        };
+    }
+    public MapDirection previous() {
+        return switch (this) {
+            case NORTH -> WEST;
+            case EAST -> NORTH;
+            case SOUTH -> EAST;
+            case WEST -> SOUTH;
+        };
+    }
+    public Vector2d toUnitVector () {
+        return switch (this){
+            case NORTH -> new Vector2d(0,1);
+            case EAST -> new Vector2d(1,0);
+            case SOUTH -> new Vector2d(0,-1);
+            case WEST -> new Vector2d(-1,0);
+
+
+        };
+    }
+}
+
