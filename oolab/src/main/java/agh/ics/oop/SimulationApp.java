f56a5972660e3f0d1f5dfb10c97a686448805a00
Wojciech Palczewski <w.k.palczewski@gmail.com>
2025-12-19 00:48:28 +0100

    Add JavaFX App Framework

---
+++ oolab/src/main/java/agh/ics/oop/SimulationApp.java
@@ -0,0 +1,13 @@
+package agh.ics.oop;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SimulationApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+
+        primaryStage.show();
+    }
+}
